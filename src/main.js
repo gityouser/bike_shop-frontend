1b4fa4602788c6eb79ca091809d1dcf8e91d33b8
Andrei Constantinescu <andreiconstantinescu@Andrei-Constantinescu.local>
2018-09-30 16:56:53 +0300

    restructured main css, styled login and added signup, initial js logic

--- src/main.js
+++ src/main.js
@@ -1,14 +1,36 @@
-const main = document.querySelector('#main');
-const headerHeight = document.getElementsByTagName('NAV')[0].offsetHeight;
-const footerHeight = document.getElementsByTagName('FOOTER')[0].offsetHeight;
-const userLoggedIn = false;
+const main = document.querySelector('#main')
+const headerHeight = document.getElementsByTagName('NAV')[0].offsetHeight
+const footerHeight = document.getElementsByTagName('FOOTER')[0].offsetHeight
+const userLoggedIn = false
+const navAddBike = document.querySelector('.addBike-btn')
+const loginTemplate = document.querySelector('#login_template')
+const signupTemplate = document.querySelector('#signup_template')
+
+
 
 // Sets main content's vertical margins so it doesn't go under the header and footer.
 function setMainMargins() {
-    main.style.marginTop = headerHeight + 'px';
-    main.style.marginBottom = footerHeight + 'px';
+    main.style.top = headerHeight + 'px'
+    main.style.bottom = footerHeight + 'px'
 }
-document.addEventListener('DOMContentLoaded', setMainMargins);
+document.addEventListener('DOMContentLoaded', setMainMargins)
+
+navAddBike.addEventListener('click', toggleLogin)
+
+function toggleLogin(e) {
+        e.preventDefault()
+        if (!userLoggedIn) {
+            const loginClone = loginTemplate.cloneNode(true)
+            main.innerHTML = loginClone.innerHTML
+    
+            const signupBtn = document.querySelector('.login_register_btn')
+            signupBtn.addEventListener('click', (e) => {
+                e.preventDefault()
+                const signupClone = signupTemplate.cloneNode(true)
+                main.innerHTML = signupClone.innerHTML
+        })
+    }
+}
 
 // renderCards takes a bike object array and renders bike cards into main content.
 function renderCards(bikes) {
