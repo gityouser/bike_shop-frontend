8baafd075fcd90566745070990a54431db63cc86
Andrei Constantinescu <andreiconstantinescu@Andrei-Constantinescu.local>
2018-09-30 10:53:35 +0300

    added box shadows, renderCards, bikeList.js

---
+++ src/bikeList.js
@@ -0,0 +1,52 @@
+const bikeList = [
+    {
+        id: 1,
+        title: 'Bicicleta cu pedale',
+        url: 'https://images.pexels.com/photos/100582/pexels-photo-100582.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+    },
+    {
+        id: 2,
+        title: 'Mountain Bike cadru aluminiu',
+        url: 'https://images.pexels.com/photos/815932/pexels-photo-815932.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+    },
+    {
+        id: 3,
+        title: 'Cursiera Racing Scott',
+        url: 'https://images.pexels.com/photos/462036/pexels-photo-462036.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+    },
+    {
+        id: 4,
+        title: 'Bicicleta cu cadru carbon',
+        url: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+    },
+    {
+        id: 5,
+        title: 'Bicicleta Lux',
+        url: 'https://images.pexels.com/photos/2884/building-vintage-bike-monument.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    },
+    {
+        id: 6,
+        title: 'Bicicleta de dama',
+        url: 'https://images.pexels.com/photos/1334642/pexels-photo-1334642.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    },
+    {
+        id: 7,
+        title: 'Brompton London',
+        url: 'https://www.brompton.com/~/media/Collabs/CHPT-3/chpt3-bike.jpg?h=200&la=en&w=266'
+    },
+    {
+        id: 8,
+        title: 'Gazelle Bike',
+        url: 'https://images.pexels.com/photos/175741/pexels-photo-175741.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    },
+    {
+        id: 9,
+        title: 'Pegas Roadster',
+        url: 'https://images.pexels.com/photos/114675/pexels-photo-114675.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    },
+    {
+        id: 10,
+        title: 'Specialised 22" frame',
+        url: 'https://images.pexels.com/photos/545004/pexels-photo-545004.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    }  
+]
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-const main = document.querySelector('#main')
+const main = document.querySelector('#main');
 const headerHeight = document.getElementsByTagName('NAV')[0].offsetHeight;
 const footerHeight = document.getElementsByTagName('FOOTER')[0].offsetHeight;
 console.log(headerHeight);
@@ -9,3 +9,24 @@ function setMainMargins() {
     main.style.marginBottom = footerHeight + 'px';
 }
 document.addEventListener('DOMContentLoaded', setMainMargins);
+
+function renderCatalog(bikes) {
+    const productList = document.querySelector('.product_list')
+    const altImg = 'http://michaelscycles-mn.com/wp-content/uploads/2015/09/BB-Logo-No-Background-300W.png'
+
+    productList.innerHTML = bikes.map( bike => {
+      return `
+        <li class="product_list_item">
+            <div>
+                <img 
+                    src="${bike.url}" 
+                    onerror="this.src='${altImg}'"
+                >
+            </div>
+            <p>${bike.title}</p>
+        </li>
+      `
+    }).join('')
+}
+
+renderCatalog(bikeList)
