c19ac0deb3f9af9e2f6f20773ed7d4d90091681c
Andrei Constantinescu <andreiconstantinescu@Andrei-Constantinescu.local>
2018-09-30 11:56:19 +0300

    login template sketch + @media mobile 2cards per row

--- src/main.js
+++ src/main.js
@@ -1,15 +1,16 @@
 const main = document.querySelector('#main');
 const headerHeight = document.getElementsByTagName('NAV')[0].offsetHeight;
 const footerHeight = document.getElementsByTagName('FOOTER')[0].offsetHeight;
-console.log(headerHeight);
-console.log(footerHeight);
+const userLoggedIn = false;
 
+// Sets main content's vertical margins so it doesn't go under the header and footer.
 function setMainMargins() {
     main.style.marginTop = headerHeight + 'px';
     main.style.marginBottom = footerHeight + 'px';
 }
 document.addEventListener('DOMContentLoaded', setMainMargins);
 
+// renderCards takes a bike object array and renders bike cards into main content.
 function renderCards(bikes) {
     const productList = document.querySelector('.product_list')
     const altImg = 'http://michaelscycles-mn.com/wp-content/uploads/2015/09/BB-Logo-No-Background-300W.png'
