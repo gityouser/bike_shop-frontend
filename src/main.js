f8b139873d09b8b7b8a422509d2c7a3e0d5f7510
Andrei Constantinescu <andreiconstantinescu@Andrei-Constantinescu.local>
2018-10-01 14:39:10 +0300

    bikes can be added now, login/logout/signup logic done

--- src/main.js
+++ src/main.js
@@ -1,13 +1,24 @@
 const main = document.querySelector('#main')
 const headerHeight = document.getElementsByTagName('NAV')[0].offsetHeight
 const footerHeight = document.getElementsByTagName('FOOTER')[0].offsetHeight
-let userLoggedIn = false
-const homeBtn = document.querySelector('.home-btn')
+const homeButtons = document.querySelectorAll('.home-btn')
 const navAddBike = document.querySelector('.addBike-btn')
 const loginTemplate = document.querySelector('#login_template')
 const signupTemplate = document.querySelector('#signup_template')
+const addBikeTemplate = document.querySelector('#addBike_template')
 const loginStatusBtn = document.querySelector('.loginStatus-btn')
+let userLoggedIn = false
 
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    setMainMargins()
+    navAddBike.addEventListener('click', addBike)
+    homeButtons.forEach(homeBtn => homeBtn.addEventListener('click', () => renderCards(bikeList)))
+    loginStatusBtn.addEventListener('click', toggleLogin)
+
+})
 
 
 // Sets main content's vertical margins so it doesn't go under the header and footer.
@@ -15,12 +26,35 @@ function setMainMargins() {
     main.style.top = headerHeight + 'px'
     main.style.bottom = footerHeight + 'px'
 }
-document.addEventListener('DOMContentLoaded', setMainMargins)
 
-navAddBike.addEventListener('click', toggleLogin)
-homeBtn.addEventListener('click', () => renderCards(bikeList))
-loginStatusBtn.addEventListener('click', toggleLogin)
+// Pushes data submitted via signup and addBike forms, into the original storage arrays
+function pushBikeData(e) {
+    e.preventDefault()
+    const form = e.target.parentElement
+    const bikeData = {
+        id: bikeList.length + 1,
+        title: form.elements[1].value,
+        url: form.elements[0].value,
+    }
+    validateSubmittedData(bikeData, form, e.target)
+}
 
+function pushUserData(data) {
+    users.push(data)
+    renderCards(bikeList)
+}
+
+function addBike(e) {
+    e.preventDefault()
+    if (userLoggedIn) {
+        const addBikeClone = addBikeTemplate.cloneNode(true)
+        main.innerHTML = addBikeClone.innerHTML;
+        const addBikeBtn = document.querySelector('.addBike_form_submit')
+        addBikeBtn.addEventListener('click', pushBikeData)
+        return;
+    }
+    toggleLogin(e)
+}
 
 // Displays the Login form
 function toggleLogin(e) {
@@ -34,19 +68,19 @@ function toggleLogin(e) {
 
         // Places listener on login button to submit credentials
         const submitLoginBtn = document.querySelector('.login_form_submit')
-        submitLoginBtn.addEventListener('click', submitData)
+        submitLoginBtn.addEventListener('click', submitDataForValidation)
     } else {
         logOut()
     }
 }
 
-function submitData(e) {
+// Submits Login and SignUp form data for validation
+function submitDataForValidation(e) {
     e.preventDefault()
     const submitLoginBtn = document.querySelector('.login_form_submit')
-    const signupBtn = document.querySelector('.login_register_btn')
+    const signupBtn = document.querySelector('.signup_form_submit')
 
     if (e.target === submitLoginBtn) {
-        console.log("It is sbmt btn")
         const loginForm = document.querySelector('.login_form');
         const loginButton = loginForm.elements[2];
         const loginCredentials = {
@@ -54,17 +88,17 @@ function submitData(e) {
             password: loginForm.elements[1].value
         }
     
-        validateCredentials(loginCredentials, loginForm, loginButton)
+        validateSubmittedData(loginCredentials, loginForm, loginButton)
     } else if (e.target === signupBtn) {
-        console.log("I'm SIGNUP  btn")
+        const signupForm = document.querySelector('.signup_form');
         const signupData = {
-            firstname: loginForm.elements[0].value,
-            lastname: loginForm.elements[1].value,
-            email: loginForm.elements[2].value,
-            password: loginForm.elements[3].value,
+            username: signupForm.elements[0].value,
+            name: signupForm.elements[1].value,
+            email: signupForm.elements[2].value,
+            password: signupForm.elements[3].value,
         }
 
-        validateCredentials(loginCredentials, loginForm, loginButton)
+        validateSubmittedData(signupData, signupForm, signupBtn)
     }
 
 }
@@ -73,25 +107,46 @@ function toggleSignup(e) {
     e.preventDefault()
     const signupClone = signupTemplate.cloneNode(true)
     main.innerHTML = signupClone.innerHTML
+    const signupBtn = document.querySelector('.signup_form_submit')
+    signupBtn.addEventListener('click', signUp)
 }
 
-function validateCredentials(data, loginForm, loginBtn) {
-    let validated = false
-    if (Object.keys(data).length === 2) {
-        users.forEach(user => {
-            if(JSON.stringify(user) === JSON.stringify(data)) { 
-                validated = true
-                 userLoggedIn = true
-                 renderCards(bikeList)
-            }
-        })
-    }
+function signUp(e) {
+    submitDataForValidation(e)
+}
 
-    if (validated) {
-        console.log('Yeey! COME ON IN!!')
-    } else {
-        displayErrorMessage(loginForm, loginBtn, 'No luck, TRY AGAIN!')
+function validateSubmittedData(data, parentForm, targetButton) {
+    // Check if it's signup data
+    if (Object.keys(data).length === 4) {
+        if (data.username.length  && data.password.length) {
+            console.log("Coolio, you're signed up")
+            pushUserData(data)
+        } else {
+            displayErrorMessage(parentForm, targetButton, 'Minimum USERNAME and password')
+        }
+        return
     }
+    // Check if it's login data
+    else if ('username' in data) {
+        console.log(data.username)
+        const index = users.findIndex(user => user.username === data.username && user.password === data.password)
+        if(index !== -1) { 
+                userLoggedIn = true
+                renderCards(bikeList)
+        } else {
+            console.log('here')
+            displayErrorMessage(parentForm, targetButton, 'No luck, TRY AGAIN!')
+        }
+    } 
+    // Check if it's addBike data
+    else if ('title' in data) { 
+        if (data.url && data.title) {
+            bikeList.push(data)
+            renderCards(bikeList)
+        } else {
+            displayErrorMessage(parentForm, targetButton, 'Add URL and Title')
+        }
+    } 
 }
 
 function displayErrorMessage(parentElement, refferenceElement, message) {
@@ -133,4 +188,5 @@ renderCards(bikeList)
 function logOut() {
     userLoggedIn = false;
     loginStatusBtn.innerText = 'Login'
+    renderCards(bikeList)
 }
